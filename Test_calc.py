e6f70ad132be2ad04ad3b3884fe1d96f73b249bb
grbi <grbi@zhaw.ch>
2020-10-22 13:38:14 +0200

    temporary: to test functions in beta nightly script

---
+++ Test_calc.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 21 20:42:53 2020
+
+@author: grbi
+"""
+import os 
+import numpy as np
+import pandas as pd
+
+import sqlalchemy as sq
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+import logging
+
+os.chdir('C:\\Users\\grbi\\PycharmProjects\\cftc_neu')
+from cftc_functions_nightly_betas import *
+
+os.chdir('C:\\Users\\grbi\\PycharmProjects\\cftc_neu\\results')
+mm_expo_sf = pd.read_excel('mm_exposure.xlsx',sheet_name ='R2_and_scalingFactor',index_col =3)
+
+
+multiplier = pd.read_sql(sq.text('select bb_tkr,multiplier from cftc.fut_mult'),c)
+multiplier = multiplier.set_index('bb_tkr')
+
+
+results = pd.DataFrame(index = multiplier.index, columns = ['R2','scalingFactor'])
+for tk in multiplier.index[4:]:
+    print(tk)
+    if tk == 'JO':
+        continue
+    x_mm = mm_expo_sf.loc[tk,'scalingFactor']/ multiplier.loc[tk,'multiplier']
+    temp_mm  = calcCFTC(type_of_exposure ='net_managed_money' ,gammatype = 'dom',alpha = ['stdev'],alpha_scale_factor=x_mm, bb_tkr = tk, start_dt='2000-12-31', end_dt='2019-12-31',regularization='d1')
+
+    w_results.loc[tk,'R2'] = temp_mm['OOSR2'].iloc[0,0]
+    w_results.loc[tk,'scalingFactor'] = x_mm
+
+
+##### Test Exposure ###### 
+# engine = sq.create_engine("postgresql+psycopg2://grbi@iwa-backtest:grbizhaw@iwa-backtest.postgres.database.azure.com:5432/postgres")
+# c = engine.connect()
+
+# w_net_mm = pd.read_sql(sq.text('select * from cftc.vw_data where px_id = 86'),c)
+# w_nonAdj = pd.read_sql(sq.text('select * from cftc.vw_data where px_id = 158'),c)
+# w_contract = pd.read_sql(sq.text('select * from cftc.vw_data where px_id = 134'),c)
