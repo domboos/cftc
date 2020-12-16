f52fe6c87cdd3a358955d2a8de7dab415ae20b2e
grbi <grbi@zhaw.ch>
2020-12-16 22:03:32 +0000

    debug Plots of Betas

--- betas_per_Model.py
+++ betas_per_Model.py
@@ -37,93 +37,89 @@ def getBetas2(model_id,betas_all, date):
     beta = betas_all[(betas_all.model_id == model_id) & (betas_all.px_date == date)]
     return beta[['return_lag','qty']]
 
-def createFigurePerModel(model_type,betas_all,dates,savefig = False):
-    #TODO: Adjust Y-Axis maybe
-    model_ids= list(betas_all.groupby('model_id').ranking.min().sort_values(ascending = True).index)
-    color = ['crimson', 'cyan'] # https://matplotlib.org/3.1.0/gallery/color/named_colors.html
-    #def Layout
-    fig, axs = plt.subplots(8, 3, sharex=False, sharey= False ,figsize=(15,20))
-    fig.tight_layout() # for nicer layout
-    fig.subplots_adjust(top=0.95)
+#%%
 
-    sns.set(font_scale = 1.2)
-    sns.set_style('white')
-    sns.set_style('white', {'font.family':'serif', 'font.serif':'Times New Roman'})
+# def createFigurePerModel(model_type,betas_all,dates,savefig = False):
+#TODO: Adjust Y-Axis maybe
+model_ids= list(betas_all.groupby('model_id').ranking.min().sort_values(ascending = True).index)
+color = ['crimson', 'cyan'] # https://matplotlib.org/3.1.0/gallery/color/named_colors.html
+#def Layout
+fig, axs = plt.subplots(8, 3, sharex=False, sharey= False ,figsize=(15,20))
+fig.tight_layout() # for nicer layout
+fig.subplots_adjust(top=0.95)
 
-    fig.suptitle(f"All Betas with model_id: {model_type}",fontsize=30)
+sns.set(font_scale = 1.2)
+sns.set_style('white')
+sns.set_style('white', {'font.family':'serif', 'font.serif':'Times New Roman'})
 
-    fig.text(0.5, 0.00, 'Return Lag', ha='center', fontsize = 20)
-    fig.text(0.00, 0.5, 'Beta', va='center', rotation='vertical', fontsize = 20)
+fig.suptitle(f"All Betas with model_id: {model_type}",fontsize=30)
+
+fig.text(0.5, 0.00, 'Return Lag', ha='center', fontsize = 20)
+fig.text(0.00, 0.5, 'Beta', va='center', rotation='vertical', fontsize = 20)
 
 
-    plot_matrix = np.arange(24).reshape(8, -1)
-    for col in range(len(plot_matrix[0])):
-        # print(f"Row: {row}")print(f"Row: {row}")
-        for row in range(len(plot_matrix)):
-            try:
-                model_id = model_ids[plot_matrix[row][col]]
-            except:
-                break
-            ax_curr = axs[row,col]
-            # ax_curr.xaxis.label.set_visible(False)
-            # ax_curr.set_xlabel()
-            # print(f"Row: {row}")
-            # print(f"col: {col}")
-            
-            k =0
-            for date in dates:
-                beta = getBetas2(model_id,betas_all,date)
-                sns.lineplot(x = beta.return_lag,y = beta.qty ,ax =ax_curr, linewidth = 4, legend = False,color =color[k])
-                k= k+1
-                # plt.xticks([])
-                # plt.yticks([])
-            ax_curr.set_xlabel('')
-            ax_curr.set_ylabel('')
-            
-            title = str(models[models.model_id == model_id].bb_tkr.values)[2:-2]
-            ax_curr.set_title(title)
+plot_matrix = np.arange(24).reshape(8, -1)
+for col in range(len(plot_matrix[0])):
+    # print(f"Row: {row}")print(f"Row: {row}")
+    for row in range(len(plot_matrix)):
+        try:
+            model_id = model_ids[plot_matrix[row][col]]
+        except:
+            break
+        ax_curr = axs[row,col]
+        # ax_curr.xaxis.label.set_visible(False)
+        # ax_curr.set_xlabel()
+        # print(f"Row: {row}")
+        # print(f"col: {col}")
+        
+        k =0
+        for date in dates:
+            beta = getBetas2(model_id,betas_all,date)
+            sns.lineplot(x = beta.return_lag,y = beta.qty ,ax =ax_curr, linewidth = 4, legend = False,color =color[k])
+            k= k+1
+            # plt.xticks([])
+            # plt.yticks([])
+        ax_curr.set_xlabel('')
+        ax_curr.set_ylabel('')
+        
+        title = str(models[models.model_id == model_id].bb_tkr.values)[2:-2]
+        ax_curr.set_title(title)
 
-    fig.delaxes(axs[7,2])
-    plt.gca().axes.get_yaxis().set_visible(False)
-    plt.show()
+fig.delaxes(axs[7,2])
 
-    # handles, labels = ax.get_legend_handles_labels()
-    fig.legend(labels=dates, bbox_to_anchor=(0.9, 0.15),fontsize = 20,frameon = False)
-    if savefig == True:
-        plt.savefig(f"/home/jovyan/work/reports/figures/Betas/{model_type}.png") #'./reports/figures/'+
+# handles, labels = ax.get_legend_handles_labels()
+fig.legend(labels=dates, bbox_to_anchor=(0.9, 0.15),fontsize = 20,frameon = False)
+plt.savefig(f"C:\\Users\\grbi\\PycharmProjects\\cftc_neu\\reports\\figures\Betas\{model_type}.png") #'./reports/figures/'+
 
-#%%sample data
-# engine1 = sq.create_engine("postgresql+psycopg2://grbi@iwa-backtest:grbizhaw@iwa-backtest.postgres.database.azure.com:5432/postgres")
-# #define Dates
-# dates = ['2015-12-29','2019-12-31']
-# model_type =40
+# %%sample data
+engine1 = sq.create_engine("postgresql+psycopg2://grbi@iwa-backtest:grbizhaw@iwa-backtest.postgres.database.azure.com:5432/postgres")
+#define Dates
+dates = ['2015-12-29','2019-12-31']
+model_type =40
 
-# df_model_type_id  = pd.read_sql_query("SELECT * FROM cftc.model_type_desc order by model_type_desc", engine1)
+df_model_type_id  = pd.read_sql_query("SELECT * FROM cftc.model_type_desc order by model_type_desc", engine1)
 
-# df_model_type_id = df_model_type_id[df_model_type_id.alpha_type.isin(['gcv','loocv'])]
-# lst_model_types = list(df_model_type_id.model_type_id)
-# betas_all,models = getSpecificData(dates,model_type= model_type)
+df_model_type_id = df_model_type_id[df_model_type_id.alpha_type.isin(['gcv','loocv'])]
+lst_model_types = list(df_model_type_id.model_type_id)
+betas_all,models = getSpecificData(dates,model_type= model_type)
 
 #%% main()
 engine1 = sq.create_engine("postgresql+psycopg2://grbi@iwa-backtest:grbizhaw@iwa-backtest.postgres.database.azure.com:5432/postgres")
 #* define Variables
 dates = ['2015-12-29','2019-12-31']
-safefig = True
+saveFig = True
 
 df_model_type_id  = pd.read_sql_query("SELECT * FROM cftc.model_type_desc order by model_type_desc", engine1)
 
+df_model_type_id = df_model_type_id[df_model_type_id.alpha_type.isin(['gcv','loocv'])]
 
-
-#%%
-    df_model_type_id = df_model_type_id[df_model_type_id.alpha_type.isin(['gcv','loocv'])]
-
-    for model_type in df_model_type_id.model_type_id:
-        print(model_type)
-        try:
-            betas_all,models = getSpecificData(dates,model_type)
-            createFigurePerModel(model_type,betas_all,dates,savefig=True)
-        except:
-            print(f"Error at: {model_type}")
+for model_type in df_model_type_id.model_type_id[0:1]:
+    print(model_type)
+    try:
+        betas_all,models = getSpecificData(dates,model_type)
+        createFigurePerModel(model_type,betas_all,dates,savefig=saveFig)
+    except:
+        print(f"Error at: {model_type}")
 
 
 
